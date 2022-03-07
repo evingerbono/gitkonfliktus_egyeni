1a306309f3879186d7f60e205a27df919a379a57
evingerbono <bono0823@gmail.com>
2022-03-07 12:24:59 +0100

    3.nev dekradalasa

---
+++ src/egyenikonfliktuskezeles/Egyenikonfliktuskezeles.java
@@ -0,0 +1,13 @@
+
+package egyenikonfliktuskezeles;
+
+public class Egyenikonfliktuskezeles {
+
+    public static void main(String[] args) {
+        
+        String nev1="valmai";
+        String nev2="masik dolog";
+        
+    }
+    
+}
